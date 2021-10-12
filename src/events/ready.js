a63c23ffe6096c67bae4d30faa523257e1454726
Defeeeee <81602924+Defeeeee@users.noreply.github.com>
2021-10-12 19:29:47 -0300

    init

---
+++ src/events/ready.js
@@ -0,0 +1,12 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+module.exports = {
+	name: 'ready',
+	once: true,
+	async execute() {
+		console.log(`Ready!`);
+	},
+
+	
+};
